74e5ca8ea61e4b95162b22dd725892bba1b9cd0c
veeru2005 <sunkavalli.veerendra1973@gmail.com>
2025-04-19 19:31:37 +0530

    ok

--- orderservice/src/main/java/com/agribzar/orderservice/repository/OrderRepository.java
+++
@@ -1,6 +0,0 @@
-package com.agribzar.orderservice.repository;
-import com.agribzar.orderservice.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
